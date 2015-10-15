9e6274aa7c356a7c2856b1f285eff1a58a2e0d1f
CasaTaloyum <casatwy@msn.com>
2015-10-15 23:26:54 +0800

    fixed a bug on whereCondition

--- CTPersistance/CTPersistance/Categories/NSString/SQL/NSString+SQL.m
+++ CTPersistance/CTPersistance/Categories/NSString/SQL/NSString+SQL.m
@@ -33,17 +33,25 @@
 
 - (NSString *)stringWithSQLParams:(NSDictionary *)params
 {
-    NSMutableString *string = [self mutableCopy];
-    [params enumerateKeysAndObjectsUsingBlock:^(NSString * _Nonnull key, NSString * _Nonnull value, BOOL * _Nonnull stop) {
-        NSString *alterKey = [NSString stringWithFormat:@":%@", key];
-        if ([value isKindOfClass:[NSString class]]) {
-            value = [value safeSQLEncode];
-            [string replaceOccurrencesOfString:alterKey withString:[NSString stringWithFormat:@"'%@'", value] options:0 range:NSMakeRange(0, string.length)];
-        } else {
-            [string replaceOccurrencesOfString:alterKey withString:[NSString stringWithFormat:@"%@", value] options:0 range:NSMakeRange(0, string.length)];
+    NSMutableArray *keyList = [[NSMutableArray alloc] init];
+    
+    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:@":[\\w]*" options:0 error:NULL];
+    NSArray *list = [expression matchesInString:self options:0 range:NSMakeRange(0, self.length)];
+    
+    [list enumerateObjectsUsingBlock:^(NSTextCheckingResult * _Nonnull checkResult, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSString *subString = [self substringWithRange:checkResult.range];
+        [keyList addObject:[subString substringWithRange:NSMakeRange(1, subString.length-1)]];
+    }];
+    
+    NSMutableString *resultString = [self mutableCopy];
+    [keyList enumerateObjectsUsingBlock:^(NSString * _Nonnull key, NSUInteger idx, BOOL * _Nonnull stop) {
+        if (params[key]) {
+            NSRegularExpression *expressionForReplace = [NSRegularExpression regularExpressionWithPattern:[NSString stringWithFormat:@":%@\\b", key] options:0 error:NULL];
+            [expressionForReplace replaceMatchesInString:resultString options:0 range:NSMakeRange(0, resultString.length) withTemplate:params[key]];
         }
     }];
-    return [string copy];
+    
+    return resultString;
 }
 
 @end
--- CTPersistance/main.m
+++ CTPersistance/main.m
@@ -14,23 +14,27 @@
 #import "TestUpdateData.h"
 #import "TestCaseRecord.h"
 #import "TestCaseTransaction.h"
+#import "TestCaseWhereCondition.h"
 
 int main(int argc, char * argv[]) {
-    TestInsertData *testInsertData = [[TestInsertData alloc] init];
-    [testInsertData test];
+//    TestInsertData *testInsertData = [[TestInsertData alloc] init];
+//    [testInsertData test];
+//    
+//    TestFetchData *testFetchData = [[TestFetchData alloc] init];
+//    [testFetchData test];
+//    
+//    TestUpdateData *testUpldateData = [[TestUpdateData alloc] init];
+//    [testUpldateData test];
+//    
+//    TestDeleteData *testDeleteData = [[TestDeleteData alloc] init];
+//    [testDeleteData test];
+//    
+//    TestCaseRecord *testCaseRecord = [[TestCaseRecord alloc] init];
+//    [testCaseRecord test];
+//    
+//    TestCaseTransaction *transaction = [[TestCaseTransaction alloc] init];
+//    [transaction test];
     
-    TestFetchData *testFetchData = [[TestFetchData alloc] init];
-    [testFetchData test];
-    
-    TestUpdateData *testUpldateData = [[TestUpdateData alloc] init];
-    [testUpldateData test];
-    
-    TestDeleteData *testDeleteData = [[TestDeleteData alloc] init];
-    [testDeleteData test];
-    
-    TestCaseRecord *testCaseRecord = [[TestCaseRecord alloc] init];
-    [testCaseRecord test];
-    
-    TestCaseTransaction *transaction = [[TestCaseTransaction alloc] init];
-    [transaction test];
+    TestCaseWhereCondition *whereCondition = [[TestCaseWhereCondition alloc] init];
+    [whereCondition test];
 }
