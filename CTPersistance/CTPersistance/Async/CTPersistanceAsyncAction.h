0d456402529150533a63ada25f5b13e2523aa734
CasaTaloyum <casatwy@msn.com>
2015-10-22 10:49:36 +0800

    added multi thread action

---
+++ CTPersistance/CTPersistance/Async/CTPersistanceAsyncAction.h
@@ -0,0 +1,13 @@
+//
+//  CTPersistanceAsyncAction.h
+//  CTPersistance
+//
+//  Created by casa on 15/10/17.
+//  Copyright © 2015年 casa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CTPersistanceAsyncAction : NSObject
+
+@end
---
+++ CTPersistance/CTPersistance/Async/CTPersistanceAsyncAction.m
@@ -0,0 +1,13 @@
+//
+//  CTPersistanceAsyncAction.m
+//  CTPersistance
+//
+//  Created by casa on 15/10/17.
+//  Copyright © 2015年 casa. All rights reserved.
+//
+
+#import "CTPersistanceAsyncAction.h"
+
+@implementation CTPersistanceAsyncAction
+
+@end
