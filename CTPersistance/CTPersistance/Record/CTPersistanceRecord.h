71bd0cf8b5c05699746a885c9feaba23a28ad495
Casa Taloyum <casatwy@msn.com>
2016-03-17 15:56:46 +0800

    added some comment

--- CTPersistance/CTPersistance/Record/CTPersistanceRecord.h
+++ CTPersistance/CTPersistance/Record/CTPersistanceRecord.h
@@ -9,8 +9,8 @@
 #import "CTPersistanceRecordProtocol.h"
 
 /**
- *  this is a record implemented by me, you can create your own Record as long as it confirms to <CTPersistanceRecordProtocol>
- *  you can just extend this class for convenience.
+ *  this is a record implemented by me, you can create your own Record as long as it confirms to <CTPersistanceRecordProtocol>, even this record is a customized UIView.
+ *  OR you can just extend this class for convenience, and use record as normal data model.
  */
 @interface CTPersistanceRecord : NSObject <CTPersistanceRecordProtocol>
 @end
