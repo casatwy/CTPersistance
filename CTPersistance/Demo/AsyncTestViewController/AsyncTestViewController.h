6a6c01899dfa17735ad623a5c59000837885bcbc
Casa Taloyum <casatwy@msn.com>
2017-08-12 18:51:18 +0800

    added Async Test View Controller

---
+++ CTPersistance/Demo/AsyncTestViewController/AsyncTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  AsyncTestViewController.h
+//  CTPersistance
+//
+//  Created by casa on 2017/8/12.
+//  Copyright © 2017年 casa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AsyncTestViewController : UIViewController
+
+@end
