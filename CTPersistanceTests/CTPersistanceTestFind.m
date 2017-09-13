410dc014b30094d0c6922d51f757d1a2da135dce
casa <casa@casahamadeMacBook-Pro.local>
2017-09-13 15:37:51 +0800

    modification

--- CTPersistanceTests/CTPersistanceTestFind.m
+++ CTPersistanceTests/CTPersistanceTestFind.m
@@ -137,11 +137,4 @@
     XCTAssertEqual(result, 3);
 }
 
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
-
 @end
--- CTPersistanceTests/CTPersistanceTestMigration.m
+++ CTPersistanceTests/CTPersistanceTestMigration.m
@@ -240,11 +240,4 @@ NSString * const kCTPersistanceMigrationTestCaseVersionKey = @"kCTPersistanceMig
     [self clean];
 }
 
-//- (void)testPerformanceExample {
-//    // This is an example of a performance test case.
-//    [self measureBlock:^{
-//        // Put the code you want to measure the time of here.
-//    }];
-//}
-
 @end
