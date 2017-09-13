eab06cba97e5a0721881e9c76bdb066ed73a9c1e
casa <casa@casahamadeMacBook-Pro.local>
2017-09-13 11:02:23 +0800

    modification

--- CTPersistance.podspec
+++ CTPersistance.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "CTPersistance"
-  s.version      = "1.3.7"
+  s.version      = "137"
   s.summary      = "Objective-C Model Layer with SQLite."
 
   s.description  = <<-DESC
--- upload.sh
+++ upload.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
+git stash
+git pull origin master --tags
+git stash pop
+
+VersionString=`grep -E 's.version.*=' CTPersistance.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+NewVersionNumber=$(($VersionNumber + 1))
+LineNumber=`grep -nE 's.version.*=' CTPersistance.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" CTPersistance.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
 pod trunk push ./CTPersistance.podspec --verbose --use-libraries
