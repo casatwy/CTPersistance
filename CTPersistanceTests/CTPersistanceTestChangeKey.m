27d28c98cf15adb2040b2797e27518b97d0ae25c
BigNerdCoding <bignerdcoding@gmail.com>
2018-05-09 11:12:55 +0800

    Revert "fix |> CTPersistanceTestChangeKey.m 中 sqlite3_key 在 Xcode9.3 下的声明问题"
    This reverts commit 77d603e2a195817ba9757bf8ffbe086b2f66ee44.

--- CTPersistanceTests/CTPersistanceTestChangeKey.m
+++ CTPersistanceTests/CTPersistanceTestChangeKey.m
@@ -11,8 +11,6 @@
 #import "CTPersistanceDataBase.h"
 #import "CTPersistanceQueryCommand+SchemaManipulations.h"
 
-//extern SQLITE_API int sqlite3_key(sqlite3 *db, const void *pKey, int nKey);
-
 @interface CTPersistanceTestChangeKey : XCTestCase
 
 @property (nonatomic, unsafe_unretained) sqlite3 *database;
