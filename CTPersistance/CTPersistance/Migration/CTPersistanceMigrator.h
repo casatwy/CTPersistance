6b9ba772a31ae2371655546d054055c67e070715
BigNerdCoding <bignerdcoding@gmail.com>
2018-06-04 21:57:12 +0800

    Revert "Merge pull request #70 from casatwy/revert-69-master"
    This reverts commit ed478511fe62dcef49360b22081bcc1e529107be, reversing
    changes made to 8ef814e5d7aa10a62a087b11d1c60c9d4cbfe104.

--- CTPersistance/CTPersistance/Migration/CTPersistanceMigrator.h
+++ CTPersistance/CTPersistance/Migration/CTPersistanceMigrator.h
@@ -98,4 +98,13 @@ extern NSString * const kCTPersistanceInitVersion;
  */
 - (void)databasePerformMigrate:(CTPersistanceDataBase *)database;
 
+/**
+ * get the database initVersion
+ *
+ * You should never call this method.
+ *
+ * @warning you should never call this method
+ */
+- (NSString *)databaseInitVersion;
+
 @end
