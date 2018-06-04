dec6330840779cc404e257c7192c452f18519cc5
BigNerdCoding <bignerdcoding@gmail.com>
2018-06-04 16:40:06 +0800

    fix |> 补充 nil 逻辑

--- CTPersistance/CTPersistance/Database/CTPersistanceDataBase.m
+++ CTPersistance/CTPersistance/Database/CTPersistanceDataBase.m
@@ -85,7 +85,13 @@ NSString * const kCTPersistanceConfigurationParamsKeyDatabaseName = @"kCTPersist
         if (isFileExistsBefore == NO) {
             CTPersistanceQueryCommand *queryCommand = [[CTPersistanceQueryCommand alloc] initWithDatabase:self];
             [[queryCommand createTable:[CTPersistanceVersionTable tableName] columnInfo:[CTPersistanceVersionTable columnInfo] error:NULL] executeWithError:NULL];
+            
             NSString *initVersion = [self.migrator databaseInitVersion];
+
+            if (!initVersion) {
+                initVersion = kCTPersistanceInitVersion;
+            }
+
             [[queryCommand insertTable:[CTPersistanceVersionTable tableName] columnInfo:[CTPersistanceVersionTable columnInfo] dataList:@[@{@"databaseVersion":initVersion}] error:NULL] executeWithError:NULL];
         }
 
